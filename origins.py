24e9eed1723ebb39cf0d7d5d3b30e6ad7d8306f1
lare <74225315+laresamdeola@users.noreply.github.com>
2023-08-24 08:26:16 +0100

    call the method to start playing the game

--- origins.py
+++ origins.py
@@ -2,6 +2,7 @@ import origins_intro
 import player_details as player
 import orisha_selection as orisha
 
+# Entry point for the game. Call this method in the python console to start playing the game. You must clone this repository first.
 
 def origins():
     origins_intro.intro()
@@ -15,17 +16,4 @@ def origins():
 
 
 #origins()
-
-# Things to work on
-# Note on the Validation -- Make it easy for players to qualify to be picked by a character
-# 1. Conclude the stages 2 and 3 for Oshun character
-# 2. Begin for Ogun and conclude
-# 3. Begin the help class/section to improve user experience
-# 4. Begin the hints section to improve user experience
-# 5. Start the Unit tests
-# 6. Begin to infuse the colorama module
-# 7. Create more asides
-# 8. Begin for Sango and conclude
-# 9. Begin for as more orisha's as possible to increase the expansiveness of the game
-# 10. Add other in game characters.
 
