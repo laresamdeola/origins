a8b34d8b0e85bca22c6ec03b2ba1220eb3a344f7
laresamdeola <74225315+laresamdeola@users.noreply.github.com>
2023-08-26 04:56:11 +0000

    fix the bug that begins the game adventure based on the element

--- orisha_selection.py
+++ orisha_selection.py
@@ -3,15 +3,12 @@ import ogun_gameplay as ogun
 
 
 def orisha_selection():
-  player_element = ""
+  element = ""
   with open("elements.txt", 'r') as elements_object:
     player_element = elements_object.readlines()
-    player_element = str(player_element[0])
-    print(type(player_element))
     print(player_element)
-    # need to fix this still
-    if player_element == "water":
-      #print(player_element)
-      oshun.oshun_gameplay()
-    elif player_element == "earth":
-      ogun.ogun_gameplay()
+    element = str(player_element[0]).strip()
+  if element == "water":
+    oshun.oshun_gameplay()
+  elif element == "earth":
+    ogun.ogun_gameplay()
